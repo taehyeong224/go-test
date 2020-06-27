3bc0e553c3c731f16118ea948b71f0b1c167cf30
kimtaehyeong <taehyeong224@gmail.com>
2020-06-27 11:10:04 +0900

    add person pkg

--- hello.go
+++ hello.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"./person"
+	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
 
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
 
+	router.GET("/person", person.Index)
+	router.GET("/person/hello/:name", person.Hello)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
